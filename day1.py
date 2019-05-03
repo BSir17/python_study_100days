075e639c5b2dca36dbca85e90d7fe4cb708624c6
BSir17 <xingbaide5@gmail.com>
2019-05-03 15:48:39 +0800

    day1

---
+++ day1.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+# Python的应用领域
+# 目前Python在云基础设施、DevOps、网络爬虫开发、数据分析挖掘、机器学习等领域都有着广泛的应用，因此也产生了Web后端开发、数据接口开发、自动化运维、自动化测试、科学计算和可视化、数据分析、量化交易、机器人开发、图像识别和处理等一系列的职位。
+
+
+#检查python版本
+import sys
+print(sys.version_info)
+print(sys.version)
+
+
+#代码中的注释
+# 单行注释 - 以#和空格开头的部分
+# 多行注释 - 三个引号开头，三个引号结尾
+
+
+#使用turtle在屏幕上绘制图形
+import turtle
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
